d2bb9ef13cc7179e8fb8a1e3d7667539a029f8d0
Akshaya192 <96040710+Akshaya192@users.noreply.github.com>
2022-02-07 17:14:15 +0000

    add two numbers

---
+++ p2original.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+int cmp(int a,int b,int c)
+{
+  if((a>b)&&(a>c))
+  printf("a is large\n");
+  else if((b>c)&&(b>a))
+  printf("b is large\n");
+  else if((c>a)&&(c>b))
+  printf("c is large");
+}
+int input()
+{
+  int a;
+  printf("enter three numbers\n");
+  scanf("%d",&a);
+  return 0;
+}
+void output(int a,int b,int c,int large)
+{
+  printf("the largeest number is:%d",large);
+}
+int main()
+{
+  int a,b,c,large;
+  a=input();
+  b=input();
+  c=input();
+  large=cmp(a,b,c);
+  return 0;
+}
+
+
+
